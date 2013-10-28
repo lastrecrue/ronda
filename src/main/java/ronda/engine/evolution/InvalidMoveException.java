b141d8a9cd6ad6f49ded6d45dff67e825e084ec3
achraf <achraf@192.168.1.15>
2013-10-28 20:32:22 +0100

    in exception set detail message with super(String message)

--- src/main/java/ronda/engine/evolution/InvalidMoveException.java
+++ src/main/java/ronda/engine/evolution/InvalidMoveException.java
@@ -5,24 +5,20 @@ import ronda.engine.elements.Move;
 public class InvalidMoveException extends Exception {
 
 	private static final long serialVersionUID = -1914617516535880319L;
-	
+
 	private Move move;
-	private String message;
 
 	public InvalidMoveException() {
 		super();
 	}
-	
+
 	public InvalidMoveException(Move move, String message) {
-		super();
-		this.message = message;
+		super(message);
+		this.move = move;
 	}
 
 	public Move getMove() {
 		return move;
 	}
 
-	public String getMessage() {
-		return message;
-	}
 }
